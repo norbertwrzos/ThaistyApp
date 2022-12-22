44acac614d14e22a68b1807516960c3f191cedbf
norbert <wrzos04@gmail.com>
2022-12-22 17:23:30 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ThaistyApp',
+      theme: ThemeData(
+        colorSchemeSeed: Colors.orange,
+        brightness: Brightness.light,
+      ),
+      home: const MyHomePage(title: 'ThaistyApp'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text("dupa"),
+      ),
+    );
+  }
+}
